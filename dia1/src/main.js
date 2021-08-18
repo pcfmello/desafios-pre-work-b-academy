bb4d8231348fe02a306ffde59c6d8a82892dc315
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2021-08-18 11:21:09 -0300

    fix: app working no errors

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
+import './immutable'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
