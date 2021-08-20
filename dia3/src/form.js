10c6fdc2c3257c916a6f680f7e9400dd0113d748
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2021-08-20 00:26:43 -0300

    feat(exercise 2): colors selectinput that are shown when select the options

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,5 +1,9 @@
+const getElement = (elementName) => document.querySelector(elementName);
+const createElement = (elementTagName) => document.createElement(elementTagName);
+
 const wordsLowercase = ["de", "da", "do", "dos"];
-const input = document.querySelector("[data-js='fullname']");
+const input = getElement("[data-js='fullname']");
+const form = getElement("[data-js='form']");
 
 const setWordsInLowercase = (words) =>
   words.map((word) => (!!word ? word.toLowerCase() : word));
@@ -25,3 +29,42 @@ input.addEventListener("input", (event) => {
   const formattedWords = setSpecificWordsToLowercase(capitalizedWords);
   input.value = formattedWords.join(" ");
 });
+
+const selectElement = createElement("select");
+selectElement.setAttribute("multiple", "");
+
+const divSelect = createElement("div");
+
+const divColors = createElement("div");
+divColors.style.display = "flex";
+
+const colors = [
+  { name: "Red", value: "#FF0000" },
+  { name: "Blue", value: "#0000FF" },
+  { name: "Green", value: "#00FF00" },
+  { name: "Yellow", value: "#FFFF00" },
+  { name: "Black", value: "#000" },
+]
+
+colors.forEach((color) => {
+  const option = createElement("option");
+  option.value = color.value;
+  option.textContent = color.name;
+  selectElement.appendChild(option);
+});
+
+
+selectElement.addEventListener("change", (event) => {
+  divColors.innerHTML = "";
+  Array.from(selectElement.selectedOptions).forEach((option) => {
+    const divColorSelected = createElement("div");
+    divColorSelected.style.width = "100px";
+    divColorSelected.style.height = "100px";
+    divColorSelected.style.backgroundColor = option.value;
+    divColors.appendChild(divColorSelected);
+  });
+});
+
+divSelect.appendChild(selectElement);
+form.appendChild(divSelect);
+form.appendChild(divColors);
